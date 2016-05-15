a9d7b8bb807959badc91b6b5b79e841a826e1aeb
ferris <jake@fusetools.com>
2016-05-14 23:19:07 -0700

    There's a monad hiding in here...

--- src/Collisions.elm
+++ src/Collisions.elm
@@ -1,23 +1,24 @@
 module Collisions exposing (collide)
 
+import List exposing (filterMap)
 import Vector exposing (..)
 import Asteroids exposing (Asteroid)
 import Bullets exposing (Bullet)
 
 collide : List Asteroid -> List Bullet -> (List Asteroid, List Bullet)
 collide asteroids bullets =
+  let (asteroids', bullets') = collide' asteroids bullets
+  in (filterMap (\x -> x) asteroids', bullets')
+
+collide' : List Asteroid -> List Bullet -> (List (Maybe Asteroid), List Bullet)
+collide' asteroids bullets =
   case asteroids of
     [] -> ([], bullets)
     x::xs ->
       let
         (x', bullets') = testAsteroid x bullets
-        (xs', bullets'') = collide xs bullets'
-      in
-        let xs'' =
-          case x' of
-            Just x'' -> x'' :: xs'
-            _ -> xs'
-        in (xs'', bullets'')
+        (xs', bullets'') = collide' xs bullets'
+      in (x' :: xs', bullets'')
 
 testAsteroid : Asteroid -> List Bullet -> (Maybe Asteroid, List Bullet)
 testAsteroid asteroid bullets =
