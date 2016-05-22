092a52345e1dfccd31a4d2363bf526b67b108779
ferris <jake@fusetools.com>
2016-05-22 14:56:31 -0700

    Added more State functions
    Also changed some formatting using core's Maybe as a model.

--- src/State.elm
+++ src/State.elm
@@ -1,4 +1,7 @@
-module State exposing (State, return, andThen, map, map2)
+module State exposing
+  ( State
+  , return, andThen
+  , map, map2, map3, map4, map5)
 
 type alias State s a = s -> (a, s)
 
@@ -18,6 +21,15 @@ map f s =
 
 map2 : (a -> b -> value) -> State s a -> State s b -> State s value
 map2 f a b = f <$> a <*> b
+
+map3 : (a -> b -> c -> value) -> State s a -> State s b -> State s c -> State s value
+map3 f a b c = f <$> a <*> b <*> c <*>
+
+map4 : (a -> b -> c -> d -> value) -> State s a -> State s b -> State s c -> State s d -> State s value
+map4 f a b c d = f <$> a <*> b <*> c <*> d
+
+map5 : (a -> b -> c -> d -> e -> value) -> State s a -> State s b -> State s c -> State s d -> State s e -> State s value
+map5 f a b c d e = f <$> a <*> b <*> c <*> d <*> e
 
 (<*>) : State s (a -> b) -> State s a -> State s b
 (<*>) f x =
